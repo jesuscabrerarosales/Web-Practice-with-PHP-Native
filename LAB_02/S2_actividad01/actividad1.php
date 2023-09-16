24bfad2c98d1cd80d48dc27c78981675d414abf0
jesuscabrerarosales <85037943+jesuscabrerarosales@users.noreply.github.com>
2023-09-15 19:49:18 -0500

    Practica de html + css + php + XAMPP

---
+++ LAB_02/S2_actividad01/actividad1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Actividad 1</title>
+	<link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+<table class="table" border="1">
+	
+	<tr>
+		<td>Edad</td>
+		<td>Peso</td>
+	</tr>
+	<tr>
+		
+	</tr>
+	<tr>
+		<td>40</td>
+		<td>78</td>
+	</tr>
+	<tr>
+		<td>53</td>
+		<td>76</td>
+	</tr>
+	
+		<td>En el momento en que entiendo verdaderamente a mi enemigo, en el momento en que le entiendo lo suficientemente bien <br>
+		como para derrotarle, entonces, en ese preciso instante, tambien le quiero</td>
+	
+</table>
+</body>
+</html>
---
+++ LAB_02/S2_ejercicio02/ejercicio2.php
@@ -0,0 +1,19 @@
+<?php
+$Frutas= ['Manzana','Uva', 'Fresa'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ejercicio 02</title>
+</head>
+<body>
+<h3>
+	La fruta numero 1 es: <?php echo $Frutas[0]?><br>
+	La fruta numero 2 es: <?php echo $Frutas[1]?><br>
+	La fruta numero 3 es: <?php echo $Frutas[2]?><br>
+</h3>
+</body>
+</html>
