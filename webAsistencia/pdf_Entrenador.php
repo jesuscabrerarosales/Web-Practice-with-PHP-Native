878327f3d7e2da0e488b6f61a9fb9f11afa9a8c5
jesuscabrerarosales <85037943+jesuscabrerarosales@users.noreply.github.com>
2023-12-26 17:26:59 -0500

    Add files via upload

---
+++ webAsistencia/pdf_Entrenador.php
@@ -0,0 +1,38 @@
+<?php
+require ('fpdf.php');
+class PDF extends FPDF{
+    function Header(){
+        $this->SetFont('Arial','B',15);
+        $this->Cell(60);
+        $this->Cell(70,10,'Reporte de Entrenadores',1,0,'C');
+        $this->Ln(20);
+
+        $this->Cell(40,10,'DNI',1,0,'C',0);
+        $this->Cell(40,10,'Nombre',1,0,'C',0);
+        $this->Cell(50,10,'Apellido Paterno',1,0,'C',0);
+        $this->Cell(50,10,'Apellido Materno',1,1,'C',0);
+    }
+    function Footer(){
+        $this->SetY(-15);
+        $this->SetFont('Arial','I',10);
+        $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    }
+}
+include 'controlador/conexion.php';
+$sentencia= $db->query("SELECT nrodocumento, nombre, apellidoPaterno,apellidoMaterno  FROM entrenador");
+$resultado= $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+
+$pdf =new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',12);
+foreach($resultado as $dato){
+    $pdf->Cell(40,10,$dato['nrodocumento'],1,0,'C',0);
+    $pdf->Cell(40,10,$dato['nombre'],1,0,'C',0);
+    $pdf->Cell(50,10,$dato['apellidoPaterno'],1,0,'C',0);
+    $pdf->Cell(50,10,$dato['apellidoMaterno'],1,1,'C',0);
+}
+
+$pdf->Output();
+?>
